153359fddb2f6bfd5db874ffa431c640233fcbfd
Shae Watson <shaelynjoy@gmail.com>
2022-07-15 14:51:15 -0700

    ++

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,43 @@
 import React from "react";
-import Map from "react-map-gl";
+import Map, { Layer } from "react-map-gl";
 
 import "./App.css";
+import precincts from "./precincts.geojson";
 
 import { MAPBOX_TOKEN } from "./tokens";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>SF Precinct Vis</p>
-      </header>
-      <section className="Map-container">
-        <Map
-          initialViewState={{
-            latitude: 37.7596,
-            longitude: -122.432,
-            zoom: 12,
-          }}
-          mapStyle="mapbox://styles/mapbox/light-v10"
-          mapboxAccessToken={MAPBOX_TOKEN}
-        ></Map>
-      </section>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <p>SF Precinct Vis</p>
+        </header>
+        <section className="Map-container">
+          <Map
+            initialViewState={{
+              latitude: 37.7596,
+              longitude: -122.432,
+              zoom: 12,
+            }}
+            mapStyle="mapbox://styles/mapbox/light-v10"
+            mapboxAccessToken={MAPBOX_TOKEN}
+          >
+            <Layer
+              {...{
+                id: "precinct_lines",
+                type: "line",
+                source: precincts,
+                paint: {
+                  "line-color": "#0075DB",
+                },
+              }}
+            />
+          </Map>
+        </section>
+      </div>
+    );
+  }
 }
 
 export default App;
